c96d3344748b5ba5d29bf1a949807947c37ba7bb
Cyntexia <Cyntexia@DESKTOP-R5AAUJH>
2023-05-10 02:23:08 +0530

    Add project files.

---
+++ Pettern program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pettern_program
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            {
+                Console.Write("Enter a number: ");
+                int n = Convert.ToInt32(Console.ReadLine());
+
+                Console.WriteLine();
+
+                for (int i = 1; i < n; i++)
+                {
+                    for (int j = 1; j <= i; j++)
+                        Console.Write(j.ToString());
+                    Console.WriteLine();
+                }
+                for (int i = n; i >= 0; i--)
+                {
+                    for (int j = 1; j <= i; j++)
+                        Console.Write(j.ToString());
+                    Console.WriteLine();
+                }
+                Console.WriteLine();
+            }
+        }
+
+    }
+}
+
